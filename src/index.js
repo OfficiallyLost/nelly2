79261451002a6e07ec82772dc9f80f689faeb9de
OfficiallyLost <farooqhaseeb49@gmail.com>
2020-06-16 15:39:01 +0100

    fix it erroring

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
-const db = require('./database/database');
+const db = require('./database/db');
 const fs = require('fs');
+const { Client } = require('eris');
+const client = new Client();
 
 db.then(() => console.log('connected to the database')).catch((e) => console.log(e));
 
-client.loadCommands();
+process.on('unhandledPromiseRejectionWarning', (e) => {
+	console.log(e);
+});
+client.commands = new Eris.Collection();
+fs.readdirSync('./commands/').forEach((dir) => {
+	const commandFiles = fs.readdirSync(`./commands/${dir}/`).filter((file) => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const command = require(`./commands/${dir}/${file}`);
+		client.commands.set(command.name, command);
+	}
+	commandFiles.forEach((f) => {
+		require(`./commands/${dir}/${f}`);
+	});
+});
 client.loadEvents();
 
 const con = async () => {
