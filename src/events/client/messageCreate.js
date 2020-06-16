3a11718bebb5cbc776736f3caf1549f516e4cf66
OfficiallyLost <farooqhaseeb49@gmail.com>
2020-06-16 04:35:04 +0100

    add cooldowns to commands

--- src/events/client/messageCreate.js
+++ src/events/client/messageCreate.js
@@ -1,3 +1,6 @@
+const eris = require('eris');
+const cooldowns = new eris.Collection();
+
 module.exports = async (client, msg) => {
 	const guildSettings = require('../../models/guild');
 	let guild = await guildSettings.findOne({ id: msg.channel.guild.id });
@@ -20,6 +23,31 @@ module.exports = async (client, msg) => {
 		client.commands.get(commandName) ||
 		client.commands.find((cmd) => cmd.aliases && cmd.aliases.includes(commandName));
 	if (!command) return;
+	if (!cooldowns.has(command.name)) {
+		cooldowns.set(command.name, new Eris.Collection());
+	}
+
+	const now = Date.now();
+	const timestamps = cooldowns.get(command.name);
+	const cooldownAmount = command.cooldown * 1000;
+
+	if (timestamps.has(message.author.id)) {
+		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+		if (now < expirationTime) {
+			const timeLeft = (expirationTime - now) / 1000;
+			return message.channel
+				.createMessage(
+					`${message.member.mention}, you're being rate limited! Please wait **${timeLeft.toFixed(
+						2
+					)}** seconds before trying to use this command.`
+				)
+				.then((e) => setTimeout(async () => await e.delete(), cooldownAmount));
+			// setTimeout(async () => {
+			// 	await r.delete();
+			// }, cooldownAmount);
+		}
+	}
 
 	try {
 		command.execute(msg, args, client);
