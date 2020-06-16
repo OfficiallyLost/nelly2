6145aa623bf2182f076f118fc14b29d1ccb67d7f
OfficiallyLost <farooqhaseeb49@gmail.com>
2020-06-16 17:22:18 +0100

    fix ping

--- src/commands/Util/ping.js
+++ src/commands/Util/ping.js
@@ -5,6 +5,6 @@ module.exports = {
 
 	execute: async (msg) => {
 		const message = await msg.channel.createMessage('Pong?');
-		message.edit(`Pong! \`${message.createdAt - msg.createdAt}ms\``);
+		message.edit(`Pong! \`${msg.createdAt - message.createdAt}ms\``);
 	}
 };
