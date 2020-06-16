96fc6db7b2a4cf6ae9005e18f1dd43c8d952d6a6
OfficiallyLost <farooqhaseeb49@gmail.com>
2020-06-16 22:38:11 +0100

    add logs onto guild model

--- src/models/guild.js
+++ src/models/guild.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 
 const guild = new mongoose.Schema({
 	id: { type: String, indexes: true },
-	prefix: { type: String }
+	prefix: { type: String },
+	logs: {
+		action: { enabled: { type: Boolean }, channel: { type: String } },
+		mod: { enabled: { type: Boolean }, channel: { type: String } }
+	}
 });
 
 module.exports = mongoose.model('guild', guild);
