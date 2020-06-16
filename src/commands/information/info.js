2ef9c749950a22ffa0ac9cd4f2950dc11d16490d
sodacova <37189894+sodacova@users.noreply.github.com>
2020-06-16 13:01:45 -0400

    created info

---
+++ src/commands/information/info.js
@@ -0,0 +1,76 @@
+module.exports = {
+	name: 'info',
+	description: 'View Nelly info',
+    cooldown: 2,
+
+execute: (msg, client) => {
+
+msg.channel.createMessage({
+    embed: {
+    title: 'Nelly',
+    thumbnail: client.user.dynamicAvatarURL,
+    color: 16283903,
+    fields: [
+        {
+    name: "Version", 
+    value: `1.0.0`, 
+    inline: true
+        },
+        {
+    name: "Library", 
+    value: `eris`,
+    inline: true,
+        },
+        {
+    name: '\u200b',
+    value: '\u200b',
+    inline: true,
+        },
+        {
+    name: "Invite",
+    value: `[Click Here](https://discordapp.com/oauth2/authorize?client_id=715862013389307976&scope=bot&permissions=2080645174)`, 
+    inline: true,
+        },
+        {
+    name: "Support Server", 
+    value: `[Click Here](https://discord.gg/VMfW2jF)`, 
+    inline: true
+        },
+        {
+    name: '\u200b', 
+    value: '\u200b', 
+    inline: true,
+        },
+        {
+    name: "Guilds", 
+    value: client.guilds.size.toLocaleString(), 
+    value: true,
+        },
+        {
+    name: "Channels", 
+    value: client.channels.size.toLocaleString(),
+    inline: true,
+        },
+        {
+    name: "Users", 
+    value: client.users.size.toLocaleString(), 
+    inline: true,
+        },
+        {
+    name: "Memory", 
+    value: `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} / 1024 MB`, 
+    inline: false,
+        },
+        {
+    name: "Developers", 
+    value: `soda#0001\nMBroken#4688\nOfficiallyLost#3484`, 
+    inline: false,
+        },
+    ],
+    footer: {
+        text: `Dev | Shard: ${client.shard} | PID: ${process.pid} | Uptime: ${duration}`
+    }
+}
+})
+}
+}
