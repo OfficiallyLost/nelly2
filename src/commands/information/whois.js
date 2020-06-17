6f2722b461af88a2f608832d6ff3b7f608eed927
OfficiallyLost <farooqhaseeb49@gmail.com>
2020-06-18 00:43:20 +0100

    works when member isnt in server now (please fix)

--- src/commands/information/whois.js
+++ src/commands/information/whois.js
@@ -6,7 +6,7 @@ module.exports = {
 	description: 'get information about a specific member',
 	cooldown: 3,
 
-	execute: (msg, args, client) => {
+	execute: async (msg, args, client) => {
 		let status = {
 			online: 'Online',
 			idle: 'Idle',
@@ -14,17 +14,15 @@ module.exports = {
 			offline: 'Offline'
 		};
 
-		const member =
-			client.users.get(args[0]) ||
-			msg.channel.guild.members.find((e) => e.mention === args.join(' ')) ||
-			msg.channel.guild.members.find((e) => e.username.toLowerCase() == args.join(' ')) ||
-			msg.channel.guild.members.find((e) => (e.nick ? e.nick.toLowerCase() : e.nick === args.join(' '))) ||
-			msg.member;
+		let member;
+		let createdAt;
 		const joinPos =
 			msg.channel.guild.members.map((m) => m).sort((a, b) => a.joinedAt - b.joinedAt).indexOf(member) + 1;
-
-		if (!member) return msg.channel.createMessage(`I could not find that member.`);
-		else
+		if (member) {
+			bits = member.publicFlags;
+			member = client.getMember(guild, args.join(' ')) || msg.member;
+			createdAt = moment(member.createdAt).tz('Europe/London').format('D MMMM YYYY h:mm:ss A');
+			console.log(member);
 			msg.channel.createMessage({
 				embed: {
 					author: {
@@ -57,7 +55,7 @@ module.exports = {
 						},
 						{
 							name: 'Registered',
-							value: moment(member.createdAt).tz('Europe/London').format('D MMMM YYYY h:mm:ss A'),
+							value: createdAt,
 							inline: false
 						}
 					],
@@ -67,5 +65,57 @@ module.exports = {
 					timestamp: new Date()
 				}
 			});
+		} else {
+			if (!args.length && !member) return;
+			try {
+				member = await client.getRESTUser(args[0]).then((e) => e);
+				const bit = member.publicFlags;
+				let badges = [];
+				if (bit & 0) badges.push('Discord Employee');
+				if (bit & 1) badges.push('Discord Partner');
+				if (bit & 2) badges.push('HypeSquad Events');
+				if (bit & 3) badges.push('Bug Hunter (Level 1)');
+				if (bit & 6) badges.push('HypeSquad Bravery');
+				if (bit & 7) badges.push('HypeSquad Brilliance');
+				if (bit & 8) badges.push('HypeSquad Balance');
+				if (bit & 9) badges.push('Early Supporter');
+				if (bit & 10) badges.push('Team User');
+				if (bit & 12) badges.push('Discord System');
+				if (bit & 14) badges.push('Bug Hunter (Level 2)');
+				if (bit & 16) badges.push('Verified Bot');
+				if (bit & 17) badges.push('Verified Bot Developer');
+				console.log(member);
+				console.log(member.id);
+				let username = member.username;
+				status = member.status;
+				createdAt = moment(member.createdAt).tz('Europe/London').format('D MMMM YYYY h:mm:ss A');
+				console.log(badges);
+				msg.channel.createMessage({
+					content: '**This user is not in the server.**',
+					embed: {
+						author: { name: member.discrim, icon_url: member.avatarURL },
+						thumbnail: { url: member.avatarURL },
+						description: badges.join(', '),
+						fields: [
+							{
+								name: 'Username',
+								value: username,
+								inline: true
+							},
+							{
+								name: 'Status',
+								value: 'status[member.status]'
+							},
+							{
+								name: 'Registered',
+								value: moment(member.createdAt).tz('Europe/London').format('D MMMM YYYY h:mm:ss A')
+							}
+						]
+					}
+				});
+			} catch (e) {
+				console.log(e);
+			}
+		}
 	}
 };
