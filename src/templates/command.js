eb806713abf0c9463f59a9703a3d32c836da362a
OfficiallyLost <farooqhaseeb49@gmail.com>
2020-06-17 14:52:12 +0100

    add templates folder

---
+++ src/templates/command.js
@@ -0,0 +1,16 @@
+module.exports = {
+	name: 'template',
+	aliases: [ 'alias' ],
+	description: 'command description',
+	examples: [ 'example' ],
+	usages: [ 'usages' ],
+	cooldown: 0,
+
+	execute: async (msg, client, args) => {
+		try {
+			msg.channel.createMessage('hi');
+		} catch (error) {
+			client.error(client, msg, error);
+		}
+	}
+};
