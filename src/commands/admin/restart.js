00c542e1ec5298010825b4e915c3b754b9c588a0
OfficiallyLost <farooqhaseeb49@gmail.com>
2020-06-17 03:50:35 +0100

    remove the unnecessary await

--- src/commands/admin/restart.js
+++ src/commands/admin/restart.js
@@ -1,33 +1,31 @@
 module.exports = {
 	name: 'restart',
 	description: 'Restart',
-    cooldown: 2,
-    
-    execute: async (msg, client) => {
+	cooldown: 2,
 
-    const process = require(`child_process`);
-    const channel = require('../../config/channels').bot;
-    let owner = ['475371795185139712', '254814547326533632', '322996242521260042']
+	execute: async (msg, client) => {
+		const process = require(`child_process`);
+		const channel = require('../../config/channels').bot;
+		let owner = [ '475371795185139712', '254814547326533632', '322996242521260042' ];
 
-    if (!owner.includes(msg.author.id))
-    return;
-    await msg.channel.createMessage({
-		embed: {
-		    description: '<a:gears:715984737163804752> Restarting',
-        }
-    }) 
+		if (!owner.includes(msg.author.id)) return;
+		await msg.channel.createMessage({
+			embed: {
+				description: '<a:gears:715984737163804752> Restarting'
+			}
+		});
 
-    await client.createMessage(channel, {
-        embed: {
-            title: 'Restart',
-            color: 12386304,
-            footer: {
-                text: 'Dev',
-            },
-            timestamp: new Date()
-        }
-    })
+		client.createMessage(channel, {
+			embed: {
+				title: 'Restart',
+				color: 12386304,
+				footer: {
+					text: 'Dev'
+				},
+				timestamp: new Date()
+			}
+		});
 
-    process.exec('pm2 restart index', (stdout, stderr) => null);
-    }
-}
+		process.exec('pm2 restart index', (stdout, stderr) => null);
+	}
+};
