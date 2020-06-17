1c4cc0d9816789a681d8d17efc1b4bf1c5b55506
OfficiallyLost <farooqhaseeb49@gmail.com>
2020-06-17 14:54:26 +0100

    unsure what i done here too

--- src/commands/admin/eval.js
+++ src/commands/admin/eval.js
@@ -3,31 +3,31 @@ module.exports = {
 	description: 'eval js code',
 	aliases: [ 'e' ],
 
-	execute: (msg, args) => {
+	execute: (msg, args, client) => {
 		const owner = require('../../config/users').devs;
-		if (owner.includes(msg.author.id))
-			try {
-				const code = args.join(' ');
-				let evaled = eval(code);
-				if (typeof evaled !== 'string') evaled = require('util').inspect(evaled);
-				const clean = (text) => {
-					if (typeof text === 'string')
-						return text
-							.replace(/`/g, '`' + String.fromCharCode(8203))
-							.replace(/@/g, '@' + String.fromCharCode(8203));
-					else return text;
-				};
-				msg.channel.createMessage(`\`\`\`js\n${clean(evaled)}\n\`\`\``);
-			} catch (err) {
-				msg.channel.createMessage({
-					embed: {
-						title: 'Error',
-						color: 13703449,
-						description: `\`\`\`xl\n${clean(err)}\n\`\`\``,
-						timestamp: new Date()
-					}
-				});
-			}
-		else console.log('no');
+		if (owner.includes(msg.author.id));
+		const clean = (text) => {
+			if (typeof text === 'string')
+				return text
+					.replace(/`/g, '`' + String.fromCharCode(8203))
+					.replace(/@/g, '@' + String.fromCharCode(8203));
+			else return text;
+		};
+		try {
+			const code = args.join(' ');
+			let evaled = eval(code);
+			if (typeof evaled !== 'string') evaled = require('util').inspect(evaled);
+
+			msg.channel.createMessage(`\`\`\`js\n${clean(evaled)}\n\`\`\``);
+		} catch (err) {
+			msg.channel.createMessage({
+				embed: {
+					title: 'Error',
+					color: 13703449,
+					description: `\`\`\`xl\n${clean(err)}\n\`\`\``,
+					timestamp: new Date()
+				}
+			});
+		}
 	}
 };
