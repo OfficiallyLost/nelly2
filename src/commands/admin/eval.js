651cbb459c5eb4be2fcb52ab33abbb7537636fe4
OfficiallyLost <farooqhaseeb49@gmail.com>
2020-06-17 04:09:06 +0100

    fix eval

--- src/commands/admin/eval.js
+++ src/commands/admin/eval.js
@@ -4,22 +4,20 @@ module.exports = {
 	aliases: [ 'e' ],
 
 	execute: (msg, args) => {
-		const clean = (text) => {
-			if (typeof text === 'string')
-				return text
-					.replace(/`/g, '`' + String.fromCharCode(8203))
-					.replace(/@/g, '@' + String.fromCharCode(8203));
-			else return text;
-		};
-
 		const owner = require('../../config/users').devs;
-		if (!owner.includes(msg.author.id))
+		if (owner.includes(msg.author.id))
 			try {
 				const code = args.join(' ');
 				let evaled = eval(code);
 				if (typeof evaled !== 'string') evaled = require('util').inspect(evaled);
-
-				msg.channel.createMessage(`\`\`\`${clean(evaled)}\`\`\``);
+				const clean = (text) => {
+					if (typeof text === 'string')
+						return text
+							.replace(/`/g, '`' + String.fromCharCode(8203))
+							.replace(/@/g, '@' + String.fromCharCode(8203));
+					else return text;
+				};
+				msg.channel.createMessage(`\`\`\`js\n${clean(evaled)}\`\`\``);
 			} catch (err) {
 				msg.channel.createMessage({
 					embed: {
@@ -30,5 +28,6 @@ module.exports = {
 					}
 				});
 			}
+		else console.log('no');
 	}
 };
