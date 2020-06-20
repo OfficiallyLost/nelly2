30ab4c7a84bdaf207cd9064f834ed56f212612e7
OfficiallyLost <farooqhaseeb49@gmail.com>
2020-06-20 12:26:06 +0100

    fixed everything apart from user resolver

--- src/commands/fun/hug.js
+++ src/commands/fun/hug.js
@@ -2,43 +2,42 @@ const fetch = require('node-fetch');
 
 module.exports = {
 	name: 'hug',
-    description: 'Hug a user',
+	description: 'Hug a user',
 	cooldown: 2,
 
-	execute: (msg, args) => {
-
-	const member =
-		msg.guild.members.cache.find((e) => e.id == args[0]) ||
-		msg.guild.members.cache.find((e) => `<@!${e.id}>` == args[0]) ||
-		msg.guild.members.cache.find((e) => e.user.username == args.join(' ')) ||
-		msg.guild.members.cache.find((e) => e.nickname == args.join(' '));
-	if (member === msg.author.bot) return msg.channel.send({ 
-		embed: { 
-			description: 'You cannot hug bot accounts!' 
-		}
-	});
-	if (member.id === msg.author.id) return msg.channel.send({
-		embed: { 
-			description: 'You cannot hug yourself!'
-		} 
-	});
-	if (!args.length) return msg.channel.send('You need to provide a member to hug!');
-	if (!member) return msg.channel.send({
-		embed: {
-			description: 'I could not find that member!'
-		} });
-	else {
-		const r = await fetch('https://some-random-api.ml/animu/hug');
-		r.json().then((e) => {
-			msg.channel.send({
+	execute: async (msg, args, client) => {
+		const member = client.getMember(msg.channel.guild, args.join(' '));
+		if (!args.length) return msg.channel.createMessage('You need to provide a member to hug!');
+		if (!member)
+			return msg.channel.createMessage({
 				embed: {
-					color: msg.member.displayColor,
-					description: `<@!${msg.author.id}> gave a big hug to <@!${member.id}>!`,
-					image: { url: e.link },
-					timestamp: new Date()
+					description: 'I could not find that member!'
 				}
 			});
-		});
+		if (member === msg.author.bot)
+			return msg.channel.createMessage({
+				embed: {
+					description: 'You cannot hug bot accounts!'
+				}
+			});
+		if (member.id === msg.author.id)
+			return msg.channel.createMessage({
+				embed: {
+					description: 'You cannot hug yourself!'
+				}
+			});
+		else {
+			const r = await fetch('https://some-random-api.ml/animu/hug');
+			r.json().then((e) => {
+				msg.channel.createMessage({
+					embed: {
+						color: msg.member.displayColor,
+						description: `<@!${msg.author.id}> gave a big hug to <@!${member.id}>!`,
+						image: { url: e.link },
+						timestamp: new Date()
+					}
+				});
+			});
+		}
 	}
-}
-}
+};
