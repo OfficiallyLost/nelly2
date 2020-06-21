b5dc87a1d4508681d1fa177bc5754c8d9d0fec6e
OfficiallyLost <farooqhaseeb49@gmail.com>
2020-06-21 22:02:33 +0100

    await deleting the message

--- src/commands/fun/nou.js
+++ src/commands/fun/nou.js
@@ -1,11 +1,10 @@
 module.exports = {
 	name: 'nou',
-    description: 'For when you really want to give someone the ultimate no u',
+	description: 'For when you really want to give someone the ultimate no u',
 	cooldown: 2,
 
-	execute: (msg, args) => {
-
-    msg.channel.createMessage("no u");
-    msg.delete();
-    }
-}
+	execute: async (msg) => {
+		msg.channel.createMessage('no u');
+		await msg.delete();
+	}
+};
